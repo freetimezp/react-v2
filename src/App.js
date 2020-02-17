1585df97dbfc32bbfa1c4a0c32f6662b69757829
freetimezp <kraftukrnet@ukr.net>
2020-02-17 21:29:54 +0200

    React-js.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,34 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-    	
+    <div>
+    	<Header />
+    	<Technologies />
     </div>
   );
 }
+
+const Header = () => {
+  return (
+    <div>
+    	<a href='#s'>Main</a>
+    	<a href='#s'>News</a>
+    	<a href='#s'>About</a>
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div>
+    	<ul>
+    		<li>css</li>
+    		<li>html</li>
+    		<li>js</li>
+    	</ul>
+    </div>
+  );
+}
+
 
 export default App;
