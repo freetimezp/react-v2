beeac65e47f14f5d10bdb6f2395de47640354527
freetimezp <kraftukrnet@ukr.net>
2020-02-24 19:44:30 +0200

    React-js. BrowserRouter.Route.

--- src/App.js
+++ src/App.js
@@ -3,21 +3,31 @@ import './App.css';
 
 import Header from './components/Header/Header.jsx';
 import Mainlist from './components/Mainlist/Mainlist.jsx';
-// import Profile from './components/Profile/Profile.jsx';
+import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
+import News from './components/News/News.jsx';
+import Music from './components/Music/Music.jsx';
+import Settings from './components/Settings/Settings.jsx';
+
+import {Route,BrowserRouter} from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-    	<Header />
-    	<div className='app-content-wrapper'>
-    		<Mainlist />
-    		<div className='content'>
-	    		{/*<Profile />*/}
-	    		<Dialogs />
-    		</div>
-    	</div>
-    </div>
+  	<BrowserRouter>
+	    <div className='app-wrapper'>
+	    	<Header />
+	    	<div className='app-content-wrapper'>
+	    		<Mainlist />
+	    		<div className='content'>
+	    			<Route path="/profile" component={Profile} />
+	    			<Route path="/dialogs" component={Dialogs} />
+	    			<Route path="/news" component={News} />
+	    			<Route path="/music" component={Music} />
+	    			<Route path="/settings" component={Settings} />
+	    		</div>
+	    	</div>
+	    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Mainlist/Mainlist.jsx
+++ src/components/Mainlist/Mainlist.jsx
@@ -5,11 +5,11 @@ import classes from './Mainlist.module.css';
 const Mainlist = () => {
 	return (
     	<div className={classes.list}>
-    		<a href='#s' className={` ${classes.item} ${classes.active} `}>Profile</a>
-    		<a href='#s' className={classes.item}>Messages</a>
-    		<a href='#s' className={classes.item}>News</a>
-    		<a href='#s' className={classes.item}>Music</a>
-    		<a href='#s' className={classes.item}>Settings</a>
+    		<a href='/profile' className={` ${classes.item} ${classes.active} `}>Profile</a>
+    		<a href='/dialogs' className={classes.item}>Messages</a>
+    		<a href='/news' className={classes.item}>News</a>
+    		<a href='/music' className={classes.item}>Music</a>
+    		<a href='/settings' className={classes.item}>Settings</a>
     	</div>
 		);
 }
