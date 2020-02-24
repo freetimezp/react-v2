a3d55d5918cc9b7c7902c2dbcd912d3300b0192e
freetimezp <kraftukrnet@ukr.net>
2020-02-24 20:23:52 +0200

    React-js. NavLink,

--- src/components/Mainlist/Mainlist.jsx
+++ src/components/Mainlist/Mainlist.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 
 import classes from './Mainlist.module.css';
 
+import {NavLink} from "react-router-dom";
+
+
 const Mainlist = () => {
 	return (
     	<div className={classes.list}>
-    		<a href='/profile' className={` ${classes.item} ${classes.active} `}>Profile</a>
-    		<a href='/dialogs' className={classes.item}>Messages</a>
-    		<a href='/news' className={classes.item}>News</a>
-    		<a href='/music' className={classes.item}>Music</a>
-    		<a href='/settings' className={classes.item}>Settings</a>
+    		<NavLink to='/profile' activeClassName={classes.active} className={` ${classes.item} ${classes.active} `}>Profile</NavLink>
+    		<NavLink to='/dialogs' activeClassName={classes.active} className={classes.item}>Messages</NavLink>
+    		<NavLink to='/news' activeClassName={classes.active} className={classes.item}>News</NavLink>
+    		<NavLink to='/music' activeClassName={classes.active} className={classes.item}>Music</NavLink>
+    		<NavLink to='/settings' activeClassName={classes.active} className={classes.item}>Settings</NavLink>
     	</div>
 		);
 }
