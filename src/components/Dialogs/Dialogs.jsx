cb588f2d0a4b9e932bafd53721c61242c765e6e3
freetimezp <kraftukrnet@ukr.net>
2020-02-26 21:21:37 +0200

    React-js.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,41 @@
 import React from 'react';
 
+import {NavLink} from "react-router-dom";
+
 import classes from './Dialogs.module.css';
+
 
 const Dialogs = () => {
 	return (
-    	<div>
-    		123
+			<div>
+				<div className={classes.title}>Dialogs \ Messages</div>
+	    	<div className={classes.dialogs}>
+	    		<div className={classes.dialogs_items}>
+	    			<div className={classes.dialog}>
+	    				<NavLink to='/dialogs/1'>Andrew</NavLink>
+	    			</div>
+	    			<div className={classes.dialog + ' ' + classes.active}>
+	    				<NavLink to='/dialogs/2'>Dmitry</NavLink>
+	    			</div>
+	    			<div className={classes.dialog}>
+	    				<NavLink to='/dialogs/3'>Sveta</NavLink>
+	    			</div>
+	    			<div className={classes.dialog}>
+	    				<NavLink to='/dialogs/4'>Sasha</NavLink>
+	    			</div>
+	    		</div>
+	    		<div className={classes.messages}>
+	    			<div className={classes.message}>
+	    				Hi!
+	    			</div>
+	    			<div className={classes.message}>
+	    				How is your live?
+	    			</div>
+	    			<div className={classes.message}>
+	    				Yo Yo
+	    			</div>
+	    		</div>
+	    	</div>
     	</div>
 		);
 }
