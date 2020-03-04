b7f67746545cb0060def17cab209ad4743ef2653
freetimezp <kraftukrnet@ukr.net>
2020-03-04 22:15:01 +0200

    React-js. Ref. onClick,

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -11,15 +11,22 @@ const MyPosts = (props) => {
       .map ( (post) => <Post message={post.message} likes={post.likes} dislikes={post.dislikes} />
   );
 
+  let newPostElement = React.createRef();
+
+  let addPost = () => {
+    let text = newPostElement.current.value;
+    alert (text);
+  }
+
 	return (
       <div>
         <h3>My posts</h3>
         <div>
           <div>
-            <textarea></textarea>
+            <textarea ref={ newPostElement }></textarea>
           </div>
           <div>
-            <button>Add post</button>
+            <button onClick={ addPost }>Add post</button>
           </div>
         </div>
         <div className={classes.posts}>
