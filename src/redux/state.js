beb5ace9e719156bca14f1b557203dc39acd3f58
freetimezp <kraftukrnet@ukr.net>
2020-04-04 17:06:53 +0300

    React-js. Reducer.

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,8 @@
+import profileReducer from './profile-reducer.js';
+import dialogsReducer from './dialogs-reducer.js';
+import friendsReducer from './friends-reducer.js';
+
+
 let store = {
 		_state: {
 			profilePage: {
@@ -46,31 +51,11 @@ let store = {
 			this._callSubscriber = observer;
 		},
 		dispatch (action) {
-			if(action.type === ADD_POST) {
-				let newPost = {
-					id: 5, 
-					message: this._state.profilePage.newPostText,
-					likes: 0,
-					dislikes: 0
-				};
-			  this._state.profilePage.postsData.push(newPost);
-			  this._state.profilePage.newPostText = '';
-			  this._callSubscriber(this._state);
-			} else if (action.type === UPDATE_NEW_POST_TEXT) {
-				this._state.profilePage.newPostText = action.newText;
-		 		this._callSubscriber(this._state);
-			} else if (action.type === ADD_MESSAGE) {
-				let newMessage = {
-					id: 6, 
-					message: this._state.dialogsPage.newMessageText
-				};
-			  this._state.dialogsPage.messagesData.push(newMessage);
-			  this._state.dialogsPage.newMessageText = '';
-			  this._callSubscriber(this._state);
-			} else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
-				this._state.dialogsPage.newMessageText = action.newText;
-		  	this._callSubscriber(this._state);
-			}
+			this._state.profilePage = profileReducer(this._state.profilePage, action);
+			this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action);
+			this._state.friendsPage = friendsReducer(this._state.friendsPage, action);
+
+			this._callSubscriber(this._state);
 		}
 }
 
