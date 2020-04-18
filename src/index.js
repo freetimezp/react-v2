6a9daba4f9fc68e539c66ce022ed30d290d989e1
freetimezp <kraftukrnet@ukr.net>
2020-04-18 19:26:08 +0300

    React-js. copyState

--- src/index.js
+++ src/index.js
@@ -10,22 +10,15 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from 'react-redux';
 
 
-let rerenderEntireTree = (state) => {
-	ReactDOM.render(
+ReactDOM.render(
 	<BrowserRouter>
 		<Provider store={store}>
 			<App 
 			  state={store.getState()} />
 		</Provider>
 	</BrowserRouter>, document.getElementById('root'));
-}
 
-rerenderEntireTree(store.getState());
 
-store.subscribe( () => {
-	let state = store.getState();
-	rerenderEntireTree(state);
-});
 
 
 // If you want your app to work offline and load faster, you can change
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -18,17 +18,22 @@ let initialSate = {
 
 const dialogsReducer = (state = initialSate, action) => {
 	switch(action.type) {
-		case ADD_MESSAGE: 
+		case ADD_MESSAGE: {
 			let newMessage = {
 					id: 6, 
 					message: state.newMessageText
 				};
-			state.messagesData.push(newMessage);
-			state.newMessageText = '';
-			return state;
-		case UPDATE_NEW_MESSAGE_TEXT: 
-			state.newMessageText = action.newText;
-			return state;
+			let stateCopy = {...state};
+			stateCopy.messagesData = [...state.messagesData];
+			stateCopy.messagesData.push(newMessage);
+			stateCopy.newMessageText = '';
+			return stateCopy;
+		}
+		case UPDATE_NEW_MESSAGE_TEXT: {
+			let stateCopy = {...state};
+			stateCopy.newMessageText = action.newText;
+			return stateCopy;
+		}
 		default:
 		  return state;
 	}
