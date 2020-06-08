6fdea1bda79c26601f40608a3cabd555bde34c27
freetimezp <kraftukrnet@ukr.net>
2020-06-08 19:11:33 +0300

    React-js. Local state,

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import classes from './ProfileInfo.module.css';
 
 import Preloader from './../../common/Preloader/Preloader.jsx';
+import ProfileStatus from './ProfileStatus.jsx';
 
 const ProfileInfo = (props) => {
 
@@ -11,31 +12,32 @@ const ProfileInfo = (props) => {
   }
 
 	return (
-    	<div>
-    		<div>
-	    		<img 
-	    			className={classes.img} 
-	    			src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKeAg6we76Eo8qIbNC275den4JCB-WozGwGAXzNClglpjTWJgT' 
-	    			alt='foto' 
-	    		/>
-    		</div>
-    		<div className={classes.profile_block}>
-    			<div className={classes.profile_image}>
-    				<img src={props.profile.photos.large} />
-    			</div>
-    			<div>
-    				<h3 className={classes.profile_title}>{props.profile.fullName}</h3>
-            <div className={classes.profile_text}>
-              <div>About me: {props.profile.aboutMe}</div>
-              <div>Contacts: {props.profile.contacts.vk}</div>
-              <div>{props.profile.contacts.twitter}</div>
-              <div>{props.profile.contacts.instagram}</div>
-              <div>web: {props.profile.contacts.github}</div>
-            </div>
-    			</div>
-    		</div>
-    	</div>
-		);
+  	<div>
+  		<div>
+    		<img 
+    			className={classes.img} 
+    			src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTKeAg6we76Eo8qIbNC275den4JCB-WozGwGAXzNClglpjTWJgT' 
+    			alt='foto' 
+    		/>
+  		</div>
+  		<div className={classes.profile_block}>
+  			<div className={classes.profile_image}>
+  				<img src={props.profile.photos.large} />
+  			</div>
+  			<div>
+  				<h3 className={classes.profile_title}>{props.profile.fullName}</h3>
+          <ProfileStatus status={"My new status!!! Yo!"} />
+          <div className={classes.profile_text}>
+            <div>About me: {props.profile.aboutMe}</div>
+            <div>Contacts: {props.profile.contacts.vk}</div>
+            <div>{props.profile.contacts.twitter}</div>
+            <div>{props.profile.contacts.instagram}</div>
+            <div>web: {props.profile.contacts.github}</div>
+          </div>
+  			</div>
+  		</div>
+  	</div>
+	);
 }
 
 export default ProfileInfo;
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class ProfileStatus extends React.Component {
+	state = {
+		editMode: false
+	}
+	activateEditMode() {
+		this.setState({
+			editMode: true
+		})
+	}
+	deactivateEditMode() {
+		this.setState({
+			editMode: false
+		})
+	}
+	render() {
+		return (
+			<div>
+				{	!this.state.editMode 
+					?	<div>
+							<span onDoubleClick={ this.activateEditMode.bind(this) }>{this.props.status}</span>
+						</div>
+					:	<div>
+							<input autoFocus value={this.props.status} onBlur={ this.deactivateEditMode.bind(this) } />
+						</div>
+				}
+			</div>
+			);
+	}
+}
+
+export default ProfileStatus;
