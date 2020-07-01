72751b565720a5fb8053fe9920999abca1610550
freetimezp <kraftukrnet@ukr.net>
2020-07-01 21:04:36 +0300

    React-js. just new button with css login\logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,21 +6,33 @@ import ClockJs from "../common/ClockJs/ClockJs.jsx";
 
 import classes from './Header.module.css';
 
-
-
 const Header = (props) => {
     return (
         <div className={classes.header}>
             <div>
-				<ClockJs />
-			</div>
+				      <ClockJs />
+			      </div>
             <div className={classes.authBlock}>
-                {props.isAuth
-                    ? <div>
-                        <div>{props.login}</div>
-                        <button onClick={props.logout}>Logout</button>
+                {
+                  props.isAuth
+                  ? <div>
+                      <div>{props.login}</div>
+                      <button onClick={props.logout} className={classes.buttonLogin}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Logout
+                      </button>
                     </div>
-                    : <NavLink to={'/login'}>Login</NavLink>}
+                  : <NavLink to={'/login'} className={classes.buttonLogin + ' ' + classes.buttonLoginUp}>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                      Login
+                    </NavLink>
+                }
             </div>
         </div>
     );
